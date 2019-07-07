9d43ea28540ef1e8a44ed86c35d583a677b2efbf
saibot777 <stefantrajkovic777@gmail.com>
2019-07-07 16:34:21 +0200

    set impl

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,16 @@
 interface UserProperties {
-    name: string;
-    age: number;
+    name?: string;
+    age?: number;
 }
 
 export class User {
     constructor(private data: UserProperties) {}
 
     get(propName: string): number | string {
-        return this.data[propName]
+        return this.data[propName];
+    }
+
+    set(update: UserProperties): void {
+        Object.assign(this.data, update)
     }
 }
