f786c86c7dad4056e62bdbf77f8dfa8ba3dd5800
saibot777 <stefantrajkovic777@gmail.com>
2019-07-07 17:27:02 +0200

    event system initiated

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,11 @@ interface UserProperties {
     age?: number;
 }
 
+type Callback = () => {};
+
 export class User {
+    events: { [key: string]: Callback[] } = {};
+
     constructor(private data: UserProperties) {}
 
     get(propName: string): number | string {
@@ -11,6 +15,10 @@ export class User {
     }
 
     set(update: UserProperties): void {
-        Object.assign(this.data, update)
+        Object.assign(this.data, update);
+    }
+
+    on(eventName: string, callback: Callback) {
+        this.events['aldsdsdsdsffsf'] = []
     }
 }
