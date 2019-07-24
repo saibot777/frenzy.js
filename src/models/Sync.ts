8d4c5ef0f21d434befd29c4b5ec48a13fd2cfe15
saibot777 <stefantrajkovic777@gmail.com>
2019-07-24 12:32:12 +0200

    sync refactored ro be generic

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,13 +1,16 @@
-import axios, { AxiosResponse, AxiosPromise } from 'axios';
-import { UserProperties } from './User';
+import axios, { AxiosPromise } from 'axios';
 import { rootUrl } from '../consts';
 
-export class Sync {
+interface HasId {
+    id: number;
+}
+
+export class Sync<T extends HasId> {
     fetch(id: number): AxiosPromise {
         return axios.get(`${rootUrl}/${id}`);      
     }
 
-    save(data: UserProperties): void {
+    save(data: T): void {
         const { id } = data;
 
         if (id) {
