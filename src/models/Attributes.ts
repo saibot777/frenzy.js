640142430c6538fda6bfb23c3a734ed8d8701194
saibot777 <stefantrajkovic777@gmail.com>
2019-07-24 14:33:16 +0200

    passtrough methods and setting data while triggering impl

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,10 +1,8 @@
 export class Attributes<T> {
     constructor(private data: T) { }
 
-    get(propName: string): number | string {
-        return this.data[propName];
-    }
-
+    get = <K extends keyof T>(key: K): T[K] => this.data[key];
+    
     set(update: T): void {
         Object.assign(this.data, update);
     }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 interface UserProperties {
     id?: string | number;
@@ -10,4 +11,26 @@ interface UserProperties {
 export class User {
     public events: Eventing = new Eventing();
     public sync: Sync<UserProperties> = new Sync<UserProperties>();
+    public attributes: Attributes<UserProperties>;
+
+    constructor(attrs: UserProperties) {
+        this.attributes = new Attributes<UserProperties>(attrs);
+    }
+
+    get on() {
+        return this.events.on;
+    }
+
+    get trigger() {
+        return this.events.trigger;
+    }
+
+    get get() {
+        return this.attributes.get;
+    }
+
+    set(update: UserProperties): void {
+        this.attributes.set(update);
+        this.events.trigger('change');
+    }
 }
