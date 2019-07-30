3bec314df48f8ac184124839c739ce9ff74f8179
saibot777 <stefantrajkovic777@gmail.com>
2019-07-30 12:59:33 +0200

    model name handling logic impl

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -13,7 +13,8 @@ export class UserForm {
 
     eventsMap(): { [key: string]: () => void } {
         return {
-            'click:.set-age': this.setAge
+            'click:.set-age': this.setAge,
+            'click:.set-name': this.onSetName
         }
     }
 
@@ -21,8 +22,10 @@ export class UserForm {
         this.model.setRandomAge();
     }
 
-    onClick(): void {
-        console.log('Clicked')
+    onSetName = (): void => {
+        const input = this.parent.querySelector('input');
+        const name = input.value;
+        this.model.set({ name });
     }
 
     template(): string {
@@ -32,6 +35,7 @@ export class UserForm {
                 <div>User name: ${this.model.get('name')}</div> 
                 <div>User age: ${this.model.get('age')}</div> 
                 <input />
+                <button class="change-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
             </div> 
         `;
