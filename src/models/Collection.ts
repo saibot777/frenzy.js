eddb611b7142284a497d2ac8a391ed6634499630
saibot777 <stefantrajkovic777@gmail.com>
2019-07-30 11:05:16 +0200

    deserialize() impl

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,13 +1,12 @@
-import { User, UserProperties } from "./User";
 import { Eventing } from './Eventing';
 import axios from 'axios';
 import { AxiosResponse } from 'axios';
 
-export class Collection {
-    models: User[] = [];
+export class Collection<T, K> {
+    models: T[] = [];
     events: Eventing = new Eventing();
 
-    constructor(public rootUrl: string) {}
+    constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
     get on() {
         return this.events.on;
@@ -20,10 +19,9 @@ export class Collection {
     fetch(): void {
         axios.get(this.rootUrl)
             .then((response: AxiosResponse) => {
-                response.data.forEach((value: UserProperties) => {
-                    const user = User.buildUser(value);
-                    this.models.push(user);
-            })
+                response.data.forEach((value: K) => {
+                    this.models.push(this.deserialize(value));
+            });
         }).catch(err => { 
             throw new Error(err) 
         });
