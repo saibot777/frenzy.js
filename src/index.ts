2f7b461e52adbd6426d6b94261bff54ef0413a3b
saibot777 <stefantrajkovic777@gmail.com>
2019-07-30 11:46:08 +0200

    initiating model into the form

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
+
+const user = User.buildUser({ name: 'John', age: 32 });
+
 const userForm = new UserForm(
-    document.getElementById('root')
+    document.getElementById('root'),
+    user
 );
 
 userForm.render();
